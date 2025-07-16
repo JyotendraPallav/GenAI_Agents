30e53c9ee6131420bb6eba5d720093706f3818dc
Jyotendra Pallav <a0262@axtria.com>
2025-07-16 23:31:51 +0530

    Initial commit - Starting Fresh

---
+++ all_agents_tutorials/Completed/Import_LLM.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import AzureChatOpenAI
+
+load_dotenv()
+
+openai_api_type = os.getenv("OPENAI_API_TYPE")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+azure_endpoint = os.getenv("AZURE_ENDPOINT")
+openai_api_version = os.getenv("OPENAI_API_VERSION")
+Conf_api_key = os.getenv("CONFLUENCES_API_KEY")
+
+# llm = ChatOpenAI(model="gpt-4o-mini", max_tokens=1000, temperature=0)
+llm = AzureChatOpenAI(
+    openai_api_key=openai_api_key,
+    openai_api_version=openai_api_version,
+    azure_endpoint=azure_endpoint,
+    openai_api_type=openai_api_type,
+    temperature=0.2,
+    model="gpt-4o-mini",
+    verbose=False,
+    max_tokens=1000,
+)
